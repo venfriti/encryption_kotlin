4dba3a520e3122f883d6429d115cf364ee7e8b97
venfriti <tolusanusi6@gmail.com>
2023-10-20 14:46:26 +0100

    help my life

---
+++ src/main/kotlin/FunctionType.kt
@@ -0,0 +1,37 @@
+
+val trick: () -> Unit = {
+    println("Do like 2 tricks")
+}
+
+val treat: () -> Unit = {
+    println("Have like 3 treats")
+}
+
+val extraTreat : (Int) -> String = {
+    "I have being tricked like $it times"
+}
+
+val cupcake : (Int) -> String = {
+    "Have a cupcake!"
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreats: ((Int) -> String)?) : () -> Unit {
+    return if (isTrick){
+        trick
+    } else {
+        if (extraTreats != null) {
+            println(extraTreats(5))
+        }
+        treat
+    }
+}
+
+fun main(){
+    val trickFunction = trickOrTreat(false) { "$it quarters" }
+    val treatFunction = trickOrTreat(true, cupcake)
+
+    trickFunction()
+    repeat(4) {
+        treatFunction()
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,10 @@ fun main() {
     val example = Example()
     val unixTime = System.currentTimeMillis();
     val lastSixDigits = unixTime % 1000000
-    val subtract = example.subtractAndLoopBack(lastSixDigits.toString())
-    val rearrangedString = example.rearrangeDigits(subtract)
+    val subtract = example.subtractAndLoopBack("123456")
+    val rearrangedString = example.rearrangeDigits("123456")
     val remappedString = example.mappedDigits(rearrangedString)
+    println("Subtracted Number: $subtract")
     println("Original Number: $lastSixDigits")
     println("Encrypted Number: $remappedString")
 }
